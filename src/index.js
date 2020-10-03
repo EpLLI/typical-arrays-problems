80ac085dbc46ee76841e91d358f459a03071a629
EpLLI <staniush.iv@gmail.com>
2020-10-03 12:46:59 +0300

    feat: add functional

--- src/index.js
+++ src/index.js
@@ -1,12 +1,39 @@
 
 exports.min = function min (array) {
-  return 0;
+  if (array===undefined||array.length==0){
+    return 0;
+  }
+  let mVal = array[0];
+    for(let i = 1; i < array.length; i++) {
+         let cur = array[i];
+       if(cur < mVal) {
+      mVal = cur;
+    }
+  }
+  return mVal ;
 }
 
 exports.max = function max (array) {
-  return 0;
+  if (array===undefined||array.length==0){
+    return 0;
+  }
+  let mVal = array[0];
+    for(let i = 1; i < array.length; i++) {
+         let cur = array[i];
+       if(cur > mVal) {
+      mVal = cur;
+    }
+}
+  return mVal ;
 }
 
 exports.avg = function avg (array) {
-  return 0;
+  if (array===undefined||array.length==0){
+    return 0;
+  }
+  let cur=0;
+  for(let i = 0; i < array.length; i++) {
+    cur += array[i];
+  }
+  return cur/array.length;
 }
